acf9b5d6d07a7e7a412e42178739472d98488572
Andrew-Zheng-000 <zhenga7485@my.sduhsd.net>
2025-11-05 19:22:57 +0000

    Code in Class Nov 5

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Global Variables
-let level, answer, score;
+let level, answer, score, playername, nameValue;
 const levelArr = document.getElementsByName("level");
 const scoreArr = [];
 date.textContent = time();
@@ -8,6 +8,16 @@ date.textContent = time();
 // Add event listeners
 playBtn.addEventListener("click",play);
 guessBtn.addEventListener("click",makeGuess);
+playerName.addEventListener("click",enterName)
+giveUp.addEventListener("click",giveUP)
+
+function enterName(){
+    nameValue = document.getElementById("name").value;
+    nameValue = nameValue.charAt(0).toUpperCase() + nameValue.slice(1);
+    welcome.textContent = "Hello " + nameValue + ", let us begin our game."
+    playBtn.disabled = false;
+    playerName.disabled = true;
+}
 
 function play(){
     score=0;//sets score to 0 every new game
@@ -35,18 +45,18 @@ function makeGuess(){
     }
     score++; // valid guess add 1 to score
     if (userGuess > answer){
-        msg.textContent = userGuess + " is too high! Guess Again"
+        msg.textContent = userGuess + " is too high! Guess again " + nameValue;
     }
     else if (userGuess < answer){
-        msg.textContent = userGuess +  " is too low! Guess Again"
+        msg.textContent = userGuess +  " is too low! Guess again " + nameValue;
     }
     else{
         if(score == 1){
-            msg.textContent = "WOW! You guessed the number " + answer + " first try! Amazing! Press play to play again"
+            msg.textContent = "WOW! Congrats " + nameValue + ", you guessed the number " + answer + " first try! Amazing! Press play to play again";
             updateScore();
             reset();
         }else{
-            msg.textContent = "Congrats! You guessed the number " + answer + " in " + score + " tries! Press play to play again"
+            msg.textContent = "Congrats " + nameValue + "! You guessed the number " + answer + " in " + score + " tries! Press play to play again";
             updateScore();
             reset();
         }
@@ -59,24 +69,25 @@ function reset(){
     guess.value = "";
     playBtn.disabled = false;
     guess.placeholder = "";
+    giveUp.disabled = true;
     for(let i=0; i<levelArr.length; i++){
             levelArr[i].disabled=false;
     }
 }
 
 function updateScore(){
-    scoreArr.push(score);
-    scoreArr.sort((a,b)=>a-b); //sort increasing order
+    scoreArr.push({ score: score, name: nameValue });
+    scoreArr.sort((a, b) => a.score - b.score);
     let lb = document.getElementsByName("leaderboard");
     wins.textContent = "Total wins: " + scoreArr.length;
     let sum = 0;
-    for(let i=0; i<scoreArr.length;i++){
-        sum += scoreArr[i];
-        if(i<lb.length){
-            lb[i].textContent = scoreArr[i];
+    for (let i = 0; i < scoreArr.length; i++) {
+        sum += scoreArr[i].score;
+        if (i < lb.length) {
+            lb[i].textContent = scoreArr[i].score + "  " + scoreArr[i].name;
         }
     }
-    let avg = sum/scoreArr.length;
+    let avg = sum / scoreArr.length;
     avgScore.textContent = "Average Score: " + avg.toFixed(2);
 }
 
@@ -84,4 +95,11 @@ function time(){
     let d = new Date();
     // concatenate a string with al the date info...
     return d;
+}
+
+function giveUP(){
+    msg.textContent = "The answer was " + answer + ". You gave up you loser :( Try again " + nameValue;
+    score = level;
+    updateScore();
+    reset();
 }
